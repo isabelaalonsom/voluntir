e4b8a84ee32f3fb84f8a7f252105d38b6e72eebe
Isabela Alonso <isabela.alonso@gertec.com.br>
2020-11-30 14:25:21 -0300

    Tentativa de pegar dados do Firebase para alimentar os textviews da MinhaContaOng

--- app/src/main/java/br/com/voluntir/ong/MinhaContaONGActivity.java
+++ app/src/main/java/br/com/voluntir/ong/MinhaContaONGActivity.java
@@ -5,21 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.voluntir.voluntario.CadastroVoluntarioActivity;
 import br.com.voluntir.voluntir.R;
 
-public class MinhaContaONGActivity extends AppCompatActivity {
+public class MinhaContaONGActivity extends AppCompatActivity implements ValueEventListener {
 
-    private TextView txtNomeOng;
-    private TextView txtCnpj;
+    TextView txtNomeOng;
+    TextView txtCnpj;
     private TextView txtLocalizacao;
     private TextView txtCausas;
     private TextView txtTelefone;
     private TextView txtSite;
     private TextView txtEmail;
     private TextView txtResumoOng;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference("ong");
+    private DatabaseReference nomeOngDatabase = databaseReference.child("nome");
+    private DatabaseReference cnpjOngDatabase = databaseReference.child("cnpj");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +47,8 @@ public class MinhaContaONGActivity extends AppCompatActivity {
         txtSite = findViewById(R.id.txtViewSiteVariavel);
         txtEmail = findViewById(R.id.txtViewEmailVariavel);
         txtResumoOng = findViewById(R.id.txtViewResumoOngVariavel);
-
     }
+
 
     public void clicarBotaoEditarOng(View view) {
 
@@ -72,4 +83,35 @@ public class MinhaContaONGActivity extends AppCompatActivity {
         this.finishAffinity();
     }
 
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+        if (dataSnapshot.getValue(String.class)!=null) {
+
+            String key = dataSnapshot.getKey();
+            if (key.equals("nome")) {
+                String txtViewONGVariavel = dataSnapshot.getValue(String.class);
+                txtNomeOng.setText(txtViewONGVariavel);
+            }
+            if (key.equals("cnpj")) {
+                String txtViewCnpjVariavel = dataSnapshot.getValue(String.class);
+                txtCnpj.setText(txtViewCnpjVariavel);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nomeOngDatabase.addValueEventListener(this);
+        cnpjOngDatabase.addValueEventListener(this);
+    }
 }
