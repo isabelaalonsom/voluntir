03204382ad2cde0014e78978e0c28b86a7654e65
César Lúcio <73946296+cesarlucio@users.noreply.github.com>
2020-12-01 22:08:05 -0300

    tentativa de arrumar o boolean de cadastro ong que não retorna true quando cadastra e começando a recria a tela pro voluntario se cadastrar na vaga

---
+++ app/src/main/java/br/com/voluntir/voluntir/VoluntarioVisualizarVaga.java
@@ -0,0 +1,14 @@
+package br.com.voluntir.voluntir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoluntarioVisualizarVaga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voluntario_visualizar_vaga);
+    }
+}
