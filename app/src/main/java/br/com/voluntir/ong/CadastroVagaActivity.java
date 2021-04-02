1e786929030965dd3a733c7be3a3385642dbb252
Isabela Alonso <isabela.alonso@gertec.com.br>
2021-04-02 13:38:33 -0300

    data de inicio e termino da Vaga configurado igual do cadastro do voluntario

--- app/src/main/java/br/com/voluntir/ong/CadastroVagaActivity.java
+++ app/src/main/java/br/com/voluntir/ong/CadastroVagaActivity.java
@@ -15,6 +15,8 @@ import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Calendar;
+
 import br.com.voluntir.BancoFirebase;
 import br.com.voluntir.controller.ControleCadastro;
 import br.com.voluntir.model.Ong;
@@ -37,6 +39,8 @@ public class CadastroVagaActivity extends AppCompatActivity {
     Ong ong;
     Voluntario voluntario;
     String idOng;
+    boolean grava = false;
+    boolean mesdiaok = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +91,30 @@ public class CadastroVagaActivity extends AppCompatActivity {
                 vaga = new Vaga();
                 controleCadastro = new ControleCadastro();
 
+                int diaInicio = 0;
+                int mesInicio = 0;
+                int anoInicio = 0;
+
+                int diaTermino = 0;
+                int mesTermino = 0;
+                int anoTermino = 0;
+
+                String dataInicioConfig = dataInicio.getText().toString();
+                if (dataInicioConfig != null) {
+                    diaInicio = Integer.parseInt(dataInicioConfig.substring(0, 2));
+                    mesInicio = Integer.parseInt(dataInicioConfig.substring(3, 5));
+                    anoInicio = Integer.parseInt(dataInicioConfig.substring(6, 10));
+                }
+
+                String dataTerminoConfig = dataTermino.getText().toString();
+                if (dataTerminoConfig != null) {
+                    diaTermino = Integer.parseInt(dataTerminoConfig.substring(0, 2));
+                    mesTermino = Integer.parseInt(dataTerminoConfig.substring(3, 5));
+                    anoTermino = Integer.parseInt(dataTerminoConfig.substring(6, 10));
+                }
+
+                int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+
                 //verifica se todos os campos foram preenchidos
                 if (especialidade.getText().toString().isEmpty() ||
                         dataInicio.getText().toString().isEmpty() || dataTermino.getText().toString().isEmpty() ||
@@ -95,7 +123,43 @@ public class CadastroVagaActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),
                             "Preencha todos os campos ",
                             Toast.LENGTH_SHORT).show();
+                } else if (anoInicio > anoTermino) {
+                    Toast.makeText(getApplicationContext(), "A data de início deve ser menor do que a data de término da Vaga", Toast.LENGTH_SHORT).show();
+                } else if ((mesInicio < 1 || mesInicio > 12) || (mesTermino < 1 || mesTermino > 12)) {
+                    Toast.makeText(getApplicationContext(),"Mês inválido ", Toast.LENGTH_SHORT).show();
+                } else if ((mesInicio == 4 || mesInicio == 6 || mesInicio == 9 || mesInicio == 11) ||
+                        (mesTermino == 4 || mesTermino == 6 || mesTermino == 9 || mesTermino == 11)) {
+                    if ((diaInicio > 30 || diaInicio < 1) || (diaTermino > 30 || diaTermino < 1)) {
+                        Toast.makeText(getApplicationContext(),"Dia inválido ",Toast.LENGTH_SHORT).show();
+                    } else {
+                        mesdiaok = true;
+                    }
+                } else if ((mesInicio == 2) || (mesTermino == 2)) {
+                    if ((diaInicio > 28 || diaInicio < 1) || (diaTermino > 28 || diaTermino < 1)) {
+                        Toast.makeText(getApplicationContext(), "Dia inválido ", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mesdiaok = true;
+                    }
+                } else if ((mesInicio == 1 || mesInicio == 3 || mesInicio == 5 || mesInicio == 7 || mesInicio == 8 || mesInicio == 10 || mesInicio == 12) ||
+                        (mesTermino == 1 || mesTermino == 3 ||mesTermino == 5 || mesTermino == 7 || mesTermino == 8 || mesTermino == 10 || mesTermino == 12)) {
+                    if ((diaInicio > 31 || diaInicio < 1) || (diaTermino > 31 || diaTermino < 1)) {
+                        Toast.makeText(getApplicationContext(), "Dia inválido ", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mesdiaok = true;
+                    }
                 } else {
+                    mesdiaok = true;
+                }
+
+                if (mesdiaok) {
+//                    if (anoInicio > anoTermino) {
+//                        Toast.makeText(getApplicationContext(), "A data de início deve ser menor do que a data de término da Vaga", Toast.LENGTH_SHORT).show();
+//                    } else {
+                        grava = true;
+                    //}
+                }
+
+                if (grava) {
                     if (ong != null) {
                         vaga.setNomeOng(ong.getNome());
                         vaga.setIdOng(ong.getIdOng());
@@ -118,7 +182,6 @@ public class CadastroVagaActivity extends AppCompatActivity {
 
 
                     controleCadastro.cadastrarVaga(vaga, tabelaBanco, getApplicationContext());
-
                 }
             }
         });
