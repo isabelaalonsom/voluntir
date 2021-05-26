586393b8e809fd8c14c34299c22d2bd8b9795b1b
César Lúcio da Silva <cesar.silva@ze.delivery>
2021-05-26 15:40:57 -0300

    coloca botão voltar na tela esqueceu a senha

--- app/src/main/java/br/com/voluntir/voluntir/EsqueceuASenhaActivity.java
+++ app/src/main/java/br/com/voluntir/voluntir/EsqueceuASenhaActivity.java
@@ -1,12 +1,17 @@
 package br.com.voluntir.voluntir;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import br.com.voluntir.controller.ControleLogin;
 
@@ -24,6 +29,9 @@ public class EsqueceuASenhaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_esqueceu_a_senha);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(" ");
+        setSupportActionBar(toolbar);
 
 
         botaoEnviar = (Button) findViewById(R.id.btnEnviar);
@@ -44,6 +52,24 @@ public class EsqueceuASenhaActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.voltar:
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
